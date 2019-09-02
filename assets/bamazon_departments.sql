d3f3f9e24b21ac04f4ee7bf95ae366e222b47ba4
Fawzi <alami42@live.com>
2019-09-02 09:39:33 -0500

    added test MySQL schema files

--- assets/bamazon_departments.sql
+++ assets/bamazon_departments.sql
@@ -16,33 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `products`
+-- Table structure for table `departments`
 --
 
-DROP TABLE IF EXISTS `products`;
+DROP TABLE IF EXISTS `departments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `products` (
-  `item_id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(45) NOT NULL,
+CREATE TABLE `departments` (
+  `department_id` int(11) NOT NULL AUTO_INCREMENT,
   `department_name` varchar(45) NOT NULL,
-  `price` float NOT NULL DEFAULT '0.1',
-  `stock_quantity` int(11) DEFAULT '10000',
-  `product_sales` float DEFAULT '0',
-  PRIMARY KEY (`item_id`),
-  KEY `FK_departmentName` (`department_name`),
-  CONSTRAINT `FK_departmentName` FOREIGN KEY (`department_name`) REFERENCES `departments` (`department_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `over_head_costs` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`department_id`),
+  UNIQUE KEY `department_name_UNIQUE` (`department_name`),
+  UNIQUE KEY `department_id_UNIQUE` (`department_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `products`
+-- Dumping data for table `departments`
 --
 
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Blender','Appliances',349.99,100,2799.92),(2,'Toaster Oven','Appliances',164.39,100,19033),(3,'Air Fryer','Appliances',107.98,100,11471),(4,'Hand Vaccum','Appliances',39.42,20,14987.4),(5,'Coffee Maker','Appliances',157.95,100,16871),(6,'Pressure cooker','Appliances',89.96,100,13589),(7,'Refrigerator','Appliances',2100,100,17328),(8,'Electric can Opener','Appliances',29.85,100,15878),(9,'Food Processor','Appliances',44.95,100,17405),(10,'Carpet Cleaner','Appliances',188.99,99,19584),(27,'iMac','Computers',1029.99,100,14760),(28,'MacBook Air','Computers',749.99,500,15615),(29,'Shoes','Clothing',160,118,480),(30,'Speakers','Electronics',99,20,0);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'Appliances',120000),(2,'Computers',250000),(10,'Clothing',1000000),(21,'Electronics',1500000),(22,'Automotive',100000),(23,'Furniture',10000);
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
